707ac77067a5da14bc07d1994113a9633508cbe8
tetianakhytra <tekh0001@stud.kea.dk>
2025-09-08 13:02:19 +0200

    done

--- profanity_filter/js.js
+++ profanity_filter/js.js
@@ -16,15 +16,20 @@
       let changed = false;
 
       replacements.forEach(pair => {
+        //Loops through every element in the replacements array.
+//Each element (pair) is an object with two properties: bad (the word to replace) and good (the word to replace it with).
         const regex = new RegExp(`\\b${pair.bad}\\b`, "gi");
         if (regex.test(text)) {
           text = text.replace(regex, `<span class="highlight">${pair.good}</span>`);
+         // so it can be visually highlighted on the page
           changed = true;
         }
       });
 
       if (changed) {
         textElement.innerHTML = text;
+        //If the text was changed, it updates the HTML content of textElement.
+//If no changes were made, it shows a modal dialog
       } else {
         dialog.showModal();
       }
